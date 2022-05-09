6f53e08b328ff8553f8f9aa4e7041700032b1f10
Lars Vonk <larserikvonk@ziggo.nl>
2022-05-09 09:22:26 +0200

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,4 @@
-// import babel from "rollup-plugin-babel";
-// import resolve from "rollup-plugin-node-resolve";
-// import commonjs from "rollup-plugin-commonjs";
-// import { uglify } from "rollup-plugin-uglify";
-
-import { createBasicConfig } from "@open-wc/building-rollup";
+import { createBasicConfig } from '@open-wc/building-rollup';
 
 const config = createBasicConfig();
 
@@ -11,14 +6,14 @@ delete config.output.dir;
 
 export default {
 	...config,
-	input: "out-tsc/index.js",
+	input: 'out-tsc/index.js',
 	output: {
 		...config.output,
-		file: "public/bundle.js",
+		file: 'public/bundle.js'
 	},
 	watch: {
 		...config.watch,
-		include: "out-tsc/**/*",
+		include: 'out-tsc/**/*'
 	},
-	treekshake: false,
+	treekshake: false
 };
